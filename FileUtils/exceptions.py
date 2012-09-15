9553cbd63822dc360261246fc85df16ce04e5f1b
James Kent <jameschristopherkent@gmail.com>
2012-09-15 14:58:21 +0100

    Packaged Objects

---
+++ FileUtils/exceptions.py
@@ -0,0 +1,2 @@
+class FileUtilsException(Exception):
+    pass
---
+++ testhash.py
@@ -0,0 +1,8 @@
+import sys
+
+import FileUtils.hashob
+from FileUtils.hashob import *
+
+hashingobject = hasher_qt('md5','ssas')
+hashingobject.hashtype()
+hashingobject.fileaccess()
