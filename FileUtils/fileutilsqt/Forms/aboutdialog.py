449ebac2f1205f24d3cfb643f18d20379f063e62
KentJames <jameschristopherkent@gmail.com>
2012-12-29 20:52:46 +0000

    Locked size of dialog form.
    Signed-off-by: KentJames <jameschristopherkent@gmail.com>

--- FileUtils/fileutilsqt/Forms/aboutdialog.py
+++ FileUtils/fileutilsqt/Forms/aboutdialog.py
@@ -48,3 +48,124 @@ class Ui_Dialog(object):
         self.label.setText(QtGui.QApplication.translate("Dialog", "TextLabel", None, QtGui.QApplication.UnicodeUTF8))
         self.label_2.setText(QtGui.QApplication.translate("Dialog", "TextLabel", None, QtGui.QApplication.UnicodeUTF8))
 
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'aboutdialog.ui'
+#
+# Created: Sat Dec 29 20:32:22 2012
+#      by: PyQt4 UI code generator 4.9.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(462, 187)
+        Dialog.setMaximumSize(QtCore.QSize(462, 187))
+        self.label = QtGui.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(10, 10, 251, 171))
+        self.label.setFrameShape(QtGui.QFrame.Panel)
+        self.label.setFrameShadow(QtGui.QFrame.Sunken)
+        self.label.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label.setWordWrap(True)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.label_2 = QtGui.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(280, 10, 171, 171))
+        self.label_2.setFrameShape(QtGui.QFrame.Panel)
+        self.label_2.setFrameShadow(QtGui.QFrame.Sunken)
+        self.label_2.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_2.setWordWrap(True)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.line = QtGui.QFrame(Dialog)
+        self.line.setGeometry(QtCore.QRect(260, 10, 20, 171))
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setLineWidth(1)
+        self.line.setFrameShape(QtGui.QFrame.VLine)
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setObjectName(_fromUtf8("line"))
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
+        self.label.setText(_translate("Dialog", "TextLabel", None))
+        self.label_2.setText(_translate("Dialog", "TextLabel", None))
+
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'aboutdialog.ui'
+#
+# Created: Sat Dec 29 20:35:25 2012
+#      by: PyQt4 UI code generator 4.9.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(462, 187)
+        Dialog.setMinimumSize(QtCore.QSize(462, 187))
+        Dialog.setMaximumSize(QtCore.QSize(462, 187))
+        self.label = QtGui.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(10, 10, 251, 171))
+        self.label.setFrameShape(QtGui.QFrame.Panel)
+        self.label.setFrameShadow(QtGui.QFrame.Sunken)
+        self.label.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label.setWordWrap(True)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.label_2 = QtGui.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(280, 10, 171, 171))
+        self.label_2.setFrameShape(QtGui.QFrame.Panel)
+        self.label_2.setFrameShadow(QtGui.QFrame.Sunken)
+        self.label_2.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_2.setWordWrap(True)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.line = QtGui.QFrame(Dialog)
+        self.line.setGeometry(QtCore.QRect(260, 10, 20, 171))
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setLineWidth(1)
+        self.line.setFrameShape(QtGui.QFrame.VLine)
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setObjectName(_fromUtf8("line"))
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
+        self.label.setText(_translate("Dialog", "TextLabel", None))
+        self.label_2.setText(_translate("Dialog", "TextLabel", None))
+
