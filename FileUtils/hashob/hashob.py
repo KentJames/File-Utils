e9d9cdecfe4380788d642804f404491a9cffd74a
KentJames <jameschristopherkent@gmail.com>
2013-09-20 22:28:24 +0100

    Removed command line interface.

--- FileUtils/hashob/hashob.py
+++ FileUtils/hashob/hashob.py
@@ -85,7 +85,7 @@ class hasher(object):
         
         for self.block in iter(lambda: self.file.read(32768),b""):
 
-            #Update the hash algorithm with the bytes we have read
+            #Update the hash algorithm with the bytes we have read.
 
             self.check.update(self.block)
 
@@ -122,50 +122,6 @@ class hasher(object):
         
     
 
-##Class used for acting as a standalone command line hashing file.
-
-##class hasher_cli(hasher):
-##
-##    def __init__(self):
-##        
-##        hasher.__init__(self)
-##        self.args = None
-##        self.command = None
-##        
-##    def parse_commandline(self):
-##
-##        self.command = argparse.ArgumentParser(description=
-##                                               'Checksum Tool v1.1.1')
-##        self.command.add_argument('type',metavar = 'C', default = 'md5',
-##                             help = ('Specifies type of checksum. md5,'
-##                                     ' sha1, sha224, sha256, sha384'
-##                                     ' and sha512 supported.'))
-##        self.command.add_argument('file',metavar = 'F',
-##                             help = 'File to Checksum')
-##        self.args = self.command.parse_args()
-##        self.hashtype()
-##        self.fileaccess()
-##
-##    def hashtype(self):
-##
-##        try:
-##            
-##            self.check = getattr(hashlib, self.args.type)()
-##
-##        except AttributeError as msg:
-##
-##            print(msg)
-##            print(
-##"""Incorrect hash type used.
-##Use -h or --help to identify correct hash types.""")
-##            
-## Why is this here? \/
-##
-##   def fileaccess(self):
-##
-##        self.filename = self.args.file
-##        self.openfile()
-        
 
 class hasher_if(hasher):
 
@@ -192,6 +148,8 @@ class hasher_if(hasher):
 
         except AttributeError as msg:
             raise HashTypeException("hashif.hashtype")
+
+
             
         
 class hash_directory(hasher):
